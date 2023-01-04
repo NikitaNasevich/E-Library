e060ea01f9b4de4fff572f9fe22402fc3512270b
Nikita Nasevich <nasevich.nik@mail.ru>
2023-01-04 16:19:54 +0300

    create SpringConfig and MySpringMvcDispatcherSerlvetIntitializer

--- src/main/java/com/elibrary/models/Person.java
+++ src/main/java/com/elibrary/models/Person.java
@@ -1,4 +1,46 @@
 package com.elibrary.models;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public class Person {
+
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Pattern(regexp = "[A-Z]\\w+ [A-Z]\\w+ [A-Z]\\w+", message = "Keep this example: Last name First name Father's name")
+    private String fullName;
+
+    @Min(value = 1900, message = "Year of birth should be more than 1900")
+    private int yearOfBirth;
+
+    public Person(String fullName, int yearOfBirth) {
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
 }
